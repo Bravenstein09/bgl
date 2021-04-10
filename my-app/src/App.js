540480e0f67b546621fcfe66a49df4966506431c
Bravenstein <82058775+Bravenstein09@users.noreply.github.com>
2021-04-10 14:36:55 -0600

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -69,7 +69,6 @@ function App() {
         <p>This may take some time, depending on your computer. Please be patient.</p>
       </div>
       <div className="links">
-        <p>Links</p>
         <a href="https://github.com/Bravenstein09/bgl/" target="_blank">The Github Repo</a> 
 	<p>More Genres Coming Soon!</p>
       </div>
